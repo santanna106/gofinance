336edfa9aa3f1b3429556b8a234f09bdb8cd8cdf
Gabriel Santanna <gabriel.santanna@nic.intranet>
2021-12-10 10:39:47 -0300

    Definição do Tema de cores

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,17 @@
+export default  {
+    colors:{
+        primary: '#5636D3',
+        secundary:'#FF872C',
+        secundary_light: 'rgba(255,135,44,0.3)',
+        sucess: '#12A454',
+        sucess_light:'rgba(18,164,84,0.5)',
+
+        attention:'#E83F5B',
+        attention_light: 'rgba(232,63,92,0.5)',
+        shape:'#FFF',
+        title: '#363F5F',
+        text:'#969CB2',
+        background:'#F0F2F5'
+
+    }
+}
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -4,11 +4,12 @@ export const Container = styled.SafeAreaView`
     flex:1;
     justify-content:center;
     align-items:center;
-    background-color:#999;
+    background-color:${({theme}) => theme.colors.background};
+
 `;
 
 export const Title = styled.Text`
    font-size:24px;
    font-weight:bold;
-   color:purple;
+   color:${({theme}) => theme.colors.title};
 `;
