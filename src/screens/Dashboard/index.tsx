6b9dfa363f1d8e5b2e6999969a3a55e8b14c4c60
Gabriel Andrade <gabriel.santana@gmail.com>
2021-12-13 04:27:15 -0300

    Finalização da inclusão dos cartções

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
-import {Container,Title} from './styles';
+
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon,
+    HighLightCards
+} from './styles';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { HighLightCard } from '../../components/HighLightCard';
 
 export const Dashboard = () => {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{uri:'https://avatars.githubusercontent.com/u/10618397?v=4'}}/>
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Gabriel</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power"/>
+                </UserWrapper>
+            </Header>
+            <HighLightCards>
+                <HighLightCard />
+                <HighLightCard />
+                <HighLightCard />
+            </HighLightCards>
         </Container>
     )
 }
