756e67c186705269251a7e73992494dfd060463a
Gabriel Andrade <gabriel.santana@gmail.com>
2021-12-23 04:23:12 -0300

    Configuração da Tab Navigator

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import AppLoading from 'expo-app-loading';
 import {ThemeProvider} from 'styled-components';
 
+import {NavigationContainer} from '@react-navigation/native';
+
 import {
   useFonts,
   Poppins_400Regular,
@@ -11,7 +13,7 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme'
-import {Register} from './src/screens/Register';
+import {AppRoutes} from './src/routes/app.routes';
 
 
 
@@ -28,7 +30,9 @@ export default function App() {
 
   return (  
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes/>
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
