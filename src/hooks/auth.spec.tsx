e02f555d6a4f1a9cca2c1f4237c1146e4d46f18d
Gabriel Andrade <gabriel.santana@gmail.com>
2022-01-19 18:21:33 -0300

    Correção do teste com entrada customizada

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,7 +1,9 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 import fetchMock from 'jest-fetch-mock';
-
+import { mocked } from 'jest-mock';
+import { startAsync } from 'expo-auth-session';
 import { AuthProvider, useAuth } from './auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 fetchMock.enableMocks();
 
@@ -12,19 +14,24 @@ const userTest = {
   photo: 'any_photo.png'
 };
 
-jest.mock('expo-auth-session', () => {
-  return {
-    startAsync: () => ({
+jest.mock('expo-auth-session');
+
+describe('Auth Hook', () => {
+
+  beforeEach(async () => {
+    const userCollectionKey = '@gofinances:user'
+    await AsyncStorage.removeItem(userCollectionKey)
+ })
+
+  it('should be able to sign in with Google account existing', async () => {
+    const googleMocked = mocked(startAsync as any);
+    googleMocked.mockReturnValue({
       type: 'success',
       params: {
         access_token: 'any_token',
       }
-    }),
-  }
-})
+    });
 
-describe('Auth Hook', () => {
-  it('should be able to sign in with Google account existing', async () => {
     fetchMock.mockResponseOnce(JSON.stringify(userTest));
 
     const { result } = renderHook(() => useAuth(), {
@@ -35,5 +42,27 @@ describe('Auth Hook', () => {
 
     expect(result.current.user.email)
       .toBe(userTest.email);
+  });
+
+  it('user should not connect if cancel autehtication with Google', async () => {
+
+    const googleMocked = mocked(startAsync as any);
+    googleMocked.mockReturnValue({
+      type: 'cancel',
+      params: {
+        access_token: 'any_token',
+      }
+    });
+
+    
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider
+    });
+
+    await act(() => result.current.signInWithGoogle());
+
+    expect(result.current.user).not.toHaveProperty('id')
+     
   });
 });
